19c2991c0b2cfa12f9aa00ec32055154372497be
Manthan Shinde <manthan.shinde0007@gmail.com>
2024-09-05 19:11:08 +0530

    chore: Remove unused code and update UI for booking page

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -15,23 +15,6 @@ export default function IndexPage() {
   }, []);
 
   return (
-    // <div className='mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-10'>
-    //   {places.length > 0 && places.map(place => (
-    //     <div>
-    //       <div className='bg-gray-500 mb-2 rounded-2xl flex '>
-    //           {place.photos?.[0] && (
-    //             <img className='rounded-2xl object-cover aspect-square' src={"http://localhost:3000/uploads/"+place.photos[0]} alt="" />
-    //           )}
-    //       </div>
-    //       <h2 className='font-bold'>{place.title}</h2>
-    //       <h3 className='text-sm truncate '> 
-    //         {place.address}
-    //       </h3>
-    //       <h3>Price :  &#8377;{place.price}/-</h3>
-    //     </div>
-    //   ))}
-    // </div>
-    
     <div className='mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-10 '>
       {places.length > 0 && places.map(place => (
         <Link to={'/place/'+place._id} key={place._id}>
