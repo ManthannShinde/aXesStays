00d4e83772eeb391f5a61a19fe1cada0b71eadd3
Manthan Shinde <manthan.shinde0007@gmail.com>
2024-09-12 22:39:05 +0530

    Refactor API index.js to re-enable CORS origin configuration

--- api/index.js
+++ api/index.js
@@ -23,19 +23,11 @@ const jwtSecret = 'sdkjffsjdfsdjsdjccjcn';
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
-// app.use(cors({
-//     // credentials : true,
-//     // // origin : 'http://localhost:5173'
-//     // origin : 'https://a-xes-stays.vercel.app/'
-// }));
-
-const corsOptions = {
-    origin: 'https://a-xes-stays.vercel.app', // Whitelist your frontend domain
-    credentials: true // Allow cookies and other credentials
-  };
-  
-  app.use(cors(corsOptions));
-  
+app.use(cors({
+    // credentials : true,
+    // // origin : 'http://localhost:5173'
+    // origin : 'https://a-xes-stays.vercel.app/'
+}));
 
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
