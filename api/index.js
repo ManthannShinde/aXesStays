6a7d51039a4432e162e05d0623aecbe6d39edce1
Manthan Shinde <manthan.shinde0007@gmail.com>
2024-09-24 01:28:17 +0530

    Refactor CORS origin configuration in API index.js

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials : true,
     // origin : 'http://localhost:5173'
-    // origin : 'https://a-xes-stays.vercel.app'
+    origin : 'https://a-xes-stays.vercel.app'
 }));
 
 
